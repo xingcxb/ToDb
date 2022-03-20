cf04783ab70c1195bcee02360f161eb7262bb5db
easymbol <xingcxb@hotmail.com>
2022-03-21 07:52:44 +0800

    feat
    change ui

--- frontend/JS/src/App.vue
+++ frontend/JS/src/App.vue
@@ -3,7 +3,18 @@
   <!--头部-->
   <div class="header" data-wails-drag>
     <!--导航-->
+    <a-row>
+      <a-col :span="4">
+        <a-row><a-col>
 
+        </a-col></a-row>
+      </a-col>
+      <a-col :span="4"></a-col>
+      <a-col :span="4"></a-col>
+      <a-col :span="4"></a-col>
+      <a-col :span="4"></a-col>
+      <a-col :span="4"></a-col>
+    </a-row>
   </div>
 </template>
 
@@ -14,5 +25,6 @@
 .header{
   height: 64px;
   width: 100%;
+  background: #7cb305;
 }
 </style>
